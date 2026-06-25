4b0128079ee7d8871d9287bebf5fbd6003229618
kodegravity <info@kodegravity.com>
2026-06-25 19:56:56 -0400

    GHA example

---
+++ src/main/java/io/codeqube/springgha/SpringGhaApplication.java
@@ -0,0 +1,13 @@
+package io.codeqube.springgha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGhaApplication.class, args);
+    }
+
+}
